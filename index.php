268a804939cab72b4abee1a2e52e22af61fced96
Brian Faust <faustbrian@users.noreply.github.com>
2016-11-22 15:33:03 +0200

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once '../Grasp/vendor/autoload.php';
-require_once 'vendor/autoload.php';
-
-$client = new AdBeat\Client('445bb4f54fb8fd4edb6a317c37b696b4');
-
-$response = $client->api('Ad')->metrics();
-dd($response);
