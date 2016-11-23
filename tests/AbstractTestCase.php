a2989295da8b68c71d0f5e881c4b1e4c073e46cd
Brian Faust <faustbrian@users.noreply.github.com>
2016-11-23 12:35:35 +0200

    Applied fixes from StyleCI (#2)
    [ci skip] [skip ci]

--- tests/AbstractTestCase.php
+++ tests/AbstractTestCase.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of Grasp HasOffers.
+ *
+ * (c) Brian Faust <hello@brianfaust.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Grasp\HasOffers\Tests;
 
 use PHPUnit_Framework_TestCase as TestCase;
